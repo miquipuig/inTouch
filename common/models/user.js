cc986b49cc5e272345c16142ce3014987ceca8d9
Miquel Puig <mpuig22@gmail.com>
2017-08-02 10:18:29 +0000

    getbyusername

--- common/models/user.js
+++ common/models/user.js
@@ -1,15 +1,39 @@
-// Copyright IBM Corp. 2015,2016. All Rights Reserved.
-// Node module: loopback-example-access-control
-// This file is licensed under the Artistic License 2.0.
-// License text available at https://opensource.org/licenses/Artistic-2.0
 
-module.exports = function(User) {
+module.exports = function(user) {
 var re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
 
 //User.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
 
-if (!(User.settings.realmRequired || User.settings.realmDelimiter)) {
-  User.validatesUniquenessOf('email', {message: 'Email already exists'});
-  User.validatesUniquenessOf('username', {message: 'User already exists'});
+if (!(user.settings.realmRequired || user.settings.realmDelimiter)) {
+  user.validatesUniquenessOf('email', {message: 'Email already exists'});
+  user.validatesUniquenessOf('username', {message: 'User already exists'});
 }
+
+
+
+ user.getByName = function(username, cb) {
+   
+   
+    //user.findById( id, function (err, instance) {
+    user.findOne({where:{username:username}}, function (err,instance){
+        
+        var response= {};
+        if (instance !=null){
+        var response = instance;
+        //var response = "id of user is " + id;
+        }
+        cb(null, response);
+        //console.log(instance);
+    });
+  }
+
+  user.remoteMethod (
+        'getByName',
+        {
+          http: {path: '/userbyname', verb: 'get'},
+          accepts: {arg: 'username', type: 'string', http: { source: 'query' } },
+          returns: {type: 'array', root: true}
+        }
+    );
+
 };

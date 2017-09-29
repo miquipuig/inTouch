9110b8ece190867f4b2b63ecda56683b4a0042d2
Miquel Puig <mpuig22@gmail.com>
2017-09-29 12:42:48 +0000

    add thumnails

--- common/models/container.js
+++ common/models/container.js
@@ -2,4 +2,23 @@
 
 module.exports = function(Container) {
 
+    var qt = require('quickthumb');
+
+    Container.afterRemote('upload', function(ctx, res, next) {
+
+        var file = res.result.files.file[0];
+        var file_path = "./server/storage/" + file.container + "/" + file.name;
+        var file_thumb_path = "./server/storage/" + file.container + "/thumb/" + file.name;
+
+        qt.convert({
+            src: file_path,
+            dst: file_thumb_path,
+            width: 100
+        }, function (err, path) {
+
+        });
+
+        next();
+    });
+
 };

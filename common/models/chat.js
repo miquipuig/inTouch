7bf9ae3af833f0497354492e3c7bc85f565c1bd7
Miquel Puig <mpuig22@gmail.com>
2017-11-15 15:36:10 +0000

    chat implemented

--- common/models/chat.js
+++ common/models/chat.js
@@ -1,20 +1,39 @@
 'use strict';
 
 module.exports = function(Chat) {
+  var app = require('../../server/server');
   Chat.beforeRemote('create', function(context, user, next) {
+
     context.args.data.date = Date.now();
     context.args.data.clientChatId = context.req.accessToken.userId;
-    next();
+    
+
+    var users = app.models.user;
+    users.findById(context.args.data.trainerChatId, function(err, instance) {
+ 
+      if (context.args.data.trainerName == null) {
+        context.args.data.trainerName = instance.nombre;
+      }
+
+      users.findById(context.req.accessToken.userId, function(err, instance) {
+       
+        if (context.args.data.clientName == null) {
+          context.args.data.clientName = instance.nombre;;
+        }
+        next();
+      });
+
+
+    });
   });
-  
-  
-  
+
+
   Chat.beforeRemote("prototype.__create__messages", function(context, listing, next) {
 
     context.args.data.date = Date.now();
     context.args.data.userMessageId = context.req.accessToken.userId;
     next();
-});
+  });
 
 
 }

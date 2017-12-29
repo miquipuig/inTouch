849f22ae8e0ed4530083e6ba23c07561b15a825e
Miquel Puig <mpuig22@gmail.com>
2017-12-29 13:25:26 +0000

    Ip?

--- web/server/server.js
+++ web/server/server.js
@@ -4,6 +4,8 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var LoopBackContext = require('loopback-context');
 var app = module.exports = loopback();
+var os = require('os');
+
 boot(app, __dirname);
 
 app.start = function() {
@@ -111,5 +113,27 @@ app.use(function setCurrentUser(req, res, next) {
       loopbackContext.set('currentUser', user);
     }
     next();
+  });
+});
+
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
   });
 });

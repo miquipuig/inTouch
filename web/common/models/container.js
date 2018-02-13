309ededea6fc2d33023b114d12eb1af4cc9a2ec9
Miquel Puig <mpuig22@gmail.com>
2018-02-13 18:28:02 +0000

    Cambio de versión a la 1.5 de sockets

--- web/common/models/container.js
+++ web/common/models/container.js
@@ -7,17 +7,21 @@ module.exports = function(Container) {
     Container.afterRemote('upload', function(ctx, res, next) {
 
         console.log(res.result.files);
-        //res.result.files
-        var file = res.result.files.file[0];
+        res.result.files
+        
+        var file = res.result.files.fileupload[0];
+        console.log(file);
         var file_path = "./server/storage/" + file.container + "/" + file.name;
+        console.log("Aqui");
+        console.log(file_path);
         var file_thumb_path = "./server/storage/" + file.container + "/thumb/" + file.name;
 
         qt.convert({
             src: file_path,
             dst: file_thumb_path,
-            width: 100
+            width: 200
         }, function (err, path) {
-
+console.log(err);
         });
 
         next();
